31a467f0a05250ede8e9817aa848ac24e95a4bae
Reese <rlam2017@gmail.com>
2020-10-05 03:01:31 -0700

    updated mod to latest 1.16.1 forge version and mappings added reese torch with custom model and textures

--- src/main/java/com/reesecake/reesemod/blocks/ReeseTorch.java
+++ src/main/java/com/reesecake/reesemod/blocks/ReeseTorch.java
@@ -1,15 +1,37 @@
 package com.reesecake.reesemod.blocks;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.TorchBlock;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.particles.IParticleData;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+import net.minecraftforge.common.ToolType;
 
-public class ReeseTorch extends Block {
+import java.util.stream.Stream;
+
+public class ReeseTorch extends TorchBlock {
+
+    private static final VoxelShape SHAPE_N = Stream.of(
+            Block.makeCuboidShape(7, 0, 7, 9, 8, 9),
+            Block.makeCuboidShape(4, 8, 4, 12, 16, 12))
+            .reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+
     public ReeseTorch() {
-        super(AbstractBlock.Properties.create(Material.REDSTONE_LIGHT).doesNotBlockMovement()
-        );
+        super(AbstractBlock.Properties.from(Blocks.TORCH), ParticleTypes.FLAME);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE_N;
     }
 }
